1ec20396aaf0e5afb417f806d4cde1c0eeff2d45
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2024-08-03 14:14:46 +0300

    v0.0.11

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,16 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/SpellTune/",
+  base: "/SpellTune/", // Update this to your repository name
   plugins: [
     VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
       manifest: {
-        name: "My PWA App",
-        short_name: "PWA App",
-        start_url: "/SpellTune/",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#000000",
+        name: "SpellTune",
+        short_name: "SpellTune",
+        description: "A PWA built with Vite",
+        theme_color: "#ffffff",
         icons: [
           {
             src: "/SpellTune/icons/manifest-icon-192.maskable.png",
