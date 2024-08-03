32e11d94e49d2ab4ee3ba7b76e7647023dd8aaf7
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2024-08-03 13:00:04 +0300

    v0.0.3

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,11 +1,27 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker installing.");
-});
+const GHPATH = "/SpellTune";
+const APP_PREFIX = "mypwa_";
+const VERSION = "version_01";
+const URLS = [
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/icons/icon-192x192.png`,
+  `${GHPATH}/icons/icon-512x512.png`,
+  `${GHPATH}/screenshots/screenshot1.png`,
+  `${GHPATH}/screenshots/screenshot2.png`,
+];
 
-self.addEventListener("activate", (event) => {
-  console.log("Service Worker activating.");
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(APP_PREFIX + VERSION).then((cache) => {
+      return cache.addAll(URLS);
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log("Fetching:", event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
